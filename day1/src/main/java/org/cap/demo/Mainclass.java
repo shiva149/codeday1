93a534fb5b347677af34be509e951f6237120022
kushivak <kushivak@DIN73003058.corp.capgemini.com>
2018-11-23 14:50:40 +0530

    First commit

---
+++ day1/src/main/java/org/cap/demo/Mainclass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		greetings();
+	}
+
+	private static void greetings() {
+		System.out.println("welcome to git");
+		
+	}
+	 
+}
